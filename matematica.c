838904a866eebdd4a8ee1a1b73ae31804ccd9784
FilipePaco <102427696+FilipePaco@users.noreply.github.com>
2022-08-27 10:30:05 -0300

    versão 1

---
+++ matematica.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int valor1,valor2;
+  
+  printf("Digite dois números inteiros");
+  scanf("%d %d", &valor1, &valor2);
+  
+  printf("A soma desses dois valores é %d",valor1 + valor2);
+  
+  return 0;
+} 
